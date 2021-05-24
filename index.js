e2eb3cb53c79905c56d238842bbf02e96e5ac1a8
William Badger <wkbadger@gmail.com>
2021-05-24 13:38:36 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name = "Josh") {
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name = "Josh", language = "Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+function introductionWithLanguageOptional(name = "Josh", language = "Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
